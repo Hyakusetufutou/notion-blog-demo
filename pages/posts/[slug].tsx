b138af9ca88f8728c0951be49bbcb66f01c1b68c
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-03-01 20:15:11 +0900

    Slugのパスを動的になるよう変更

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,6 +1,27 @@
+import { getAllPosts, getSinglePost } from "@/lib/notionAPI";
 import React from "react";
 
-const Post = () => {
+export const getStaticPaths = async () => {
+  const allPosts = await getAllPosts();
+  const paths = allPosts.map(({ slug }) => ({ params: { slug } }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const post = await getSinglePost(params.slug);
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60 * 60,
+  };
+};
+
+const Post = ({ post }) => {
   return (
     <section className="container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20">
       <h2 className="w-full text-2xl font-medium">3回目の投稿です。</h2>
